52e89ce439b90a9ae5ed2b0cdbab077c8171bab4
jodli <job87@web.de>
2018-05-27 11:41:07 +0200

    Added comment-json package to filter out comments in snippets file.

--- extension.js
+++ extension.js
@@ -3,83 +3,7 @@ const process = require("process");
 const fs = require("fs");
 const path = require("path");
 const os = require("os");
-
-var singleComment = 1;
-var multiComment = 2;
-
-function stripWithoutWhitespace() {
-  return "";
-}
-
-function stripWithWhitespace(str, start, end) {
-  return str.slice(start, end).replace(/\S/g, " ");
-}
-
-function stripJSONComments(str, opts) {
-  opts = opts || {};
-
-  var currentChar;
-  var nextChar;
-  var insideString = false;
-  var insideComment = false;
-  var offset = 0;
-  var ret = "";
-  var strip =
-    opts.whitespace === false ? stripWithoutWhitespace : stripWithWhitespace;
-
-  for (var i = 0; i < str.length; i++) {
-    currentChar = str[i];
-    nextChar = str[i + 1];
-
-    if (!insideComment && currentChar === '"') {
-      var escaped = str[i - 1] === "\\" && str[i - 2] !== "\\";
-      if (!escaped) {
-        insideString = !insideString;
-      }
-    }
-
-    if (insideString) {
-      continue;
-    }
-
-    if (!insideComment && currentChar + nextChar === "//") {
-      ret += str.slice(offset, i);
-      offset = i;
-      insideComment = singleComment;
-      i++;
-    } else if (
-      insideComment === singleComment &&
-      currentChar + nextChar === "\r\n"
-    ) {
-      i++;
-      insideComment = false;
-      ret += strip(str, offset, i);
-      offset = i;
-      continue;
-    } else if (insideComment === singleComment && currentChar === "\n") {
-      insideComment = false;
-      ret += strip(str, offset, i);
-      offset = i;
-    } else if (!insideComment && currentChar + nextChar === "/*") {
-      ret += str.slice(offset, i);
-      offset = i;
-      insideComment = multiComment;
-      i++;
-      continue;
-    } else if (
-      insideComment === multiComment &&
-      currentChar + nextChar === "*/"
-    ) {
-      i++;
-      insideComment = false;
-      ret += strip(str, offset, i + 1);
-      offset = i + 1;
-      continue;
-    }
-  }
-
-  return ret + (insideComment ? strip(str.substr(offset)) : str.substr(offset));
-}
+const json = require("comment-json");
 
 function activate(context) {
   var disposable = vscode.commands.registerCommand(
@@ -180,7 +104,7 @@ function activate(context) {
               return;
             }
 
-            var snippets = JSON.parse(text.toString());
+            var snippets = json.parse(text.toString());
 
             if (snippets[snippetObject.name] !== undefined) {
               vscode.window.showErrorMessage(
@@ -210,7 +134,7 @@ function addSnippet(snippets, snippetObject, selectedText) {
 }
 
 function writeSnippetsToFile(snippets, userSnippetsFilePath, snippetObject) {
-  var newText = JSON.stringify(snippets, null, "\t");
+  var newText = json.stringify(snippets, null, "\t");
   fs.writeFile(userSnippetsFilePath, newText, err => {
     if (err) {
       vscode.window.showErrorMessage(
